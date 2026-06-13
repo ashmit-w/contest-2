2cd3d67882555ecb2f1948e4fe8735dde4d9756a
Ashmit Arya <ashmit@Ashmits-MacBook-Air.local>
2026-06-13 16:00:36 +0530

    Implemented Task 1

--- src/backend/dag.ts
+++ src/backend/dag.ts
@@ -4,5 +4,23 @@ export function getReadySteps(
   steps: WorkflowStep[],
   stepStatus: Record<string, StepStatus>
 ): WorkflowStep[] {
-  throw new Error("TODO: implement getReadySteps")
+  let readyArr: WorkflowStep[] = [];
+  for(let s in steps){
+    if(stepStatus[steps[s].id] === "PENDING"){
+      if (!steps[s].dependsOn) {
+        readyArr.push(steps[s]);
+      } else {
+        for(let d in steps[s].dependsOn){
+          if(stepStatus[steps[s].dependsOn[d]] === "COMPLETED"){
+            readyArr.push(steps[s]);
+          }
+        }
+      }
+    }
+  }
+  return readyArr;
 }
+
+
+
+
